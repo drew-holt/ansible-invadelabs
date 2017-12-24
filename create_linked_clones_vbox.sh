af7bbe194a8626a26c3166b5f1522710dd4c93a1
Drew Holt <drewderivative@gmail.com>
2017-12-24 05:50:29 -0700

    while loop to wait for each VM to become online instead of sleeping for 20 seconds

--- create_linked_clones_vbox.sh
+++ create_linked_clones_vbox.sh
@@ -13,15 +13,30 @@
 # VBoxManage guestproperty get ubuntu16.04-vm03 "/VirtualBox/GuestInfo/Net/1/V4/IP"
 ######################## Junk
 
+# create 4 linked clones from ubuntu1604-base snapshot 2017.12.13
 for i in {01..04}; do
   VBoxManage clonevm ubuntu1604-base --snapshot 2017.12.13 --options link --name ubuntu1604-vm$i --register;
   VBoxManage startvm --type headless ubuntu1604-vm$i;
 done
 
-sleep 20;
-
+# add this group to the top of the Ansible hosts file
 echo '[vms]' > hosts
 
+# for each VM run the while loop
 for i in {01..04}; do
-  VBoxManage guestproperty get ubuntu1604-vm$i "/VirtualBox/GuestInfo/Net/1/V4/IP" | cut -d" " -f2 >> hosts;
+    # loop until the IP and vbox guest utils are online
+    while true;
+      do
+        # NoLoggedInusers was the only property available between IP and guest tools online
+        STATE=$(VBoxManage guestproperty get ubuntu1604-vm$i "/VirtualBox/GuestInfo/OS/NoLoggedInUsers")
+        sleep 1;
+        echo "Sleeping 1 second on ubuntu1604-vm$i becoming available.";
+
+        # if "No Value set!" then the guest is up and ready to break out of the loop
+        if [ "$STATE" != "No value set!" ]; then
+          # add each host to the Ansible host list as they become online
+          VBoxManage guestproperty get ubuntu1604-vm$i "/VirtualBox/GuestInfo/Net/1/V4/IP" | cut -d" " -f2 >> hosts;
+          break
+        fi
+    done
 done
